6ac13cdd70fb35dd209c10200ae1552bff60ee88
Jason R. Clark <jclark@newrelic.com>
2013-01-06 10:38:02 -0800

    Busted when adding additional native method

--- libchimp/test.c
+++ libchimp/test.c
@@ -19,6 +19,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "chimp/array.h"
+#include "chimp/object.h"
 #include "chimp/test.h"
 #include "chimp/str.h"
 #include "chimp/class.h"
@@ -33,9 +35,56 @@ _chimp_test_init (ChimpRef *self, ChimpRef *args)
 }
 
 static ChimpRef *
-chimp_test_fail (ChimpRef *self, ChimpRef *args)
+_chimp_test_equals (ChimpRef *self, ChimpRef *args)
 {
-    fprintf (stderr, "assertion failed: on purpose!\n");
+    ChimpCmpResult r;
+    ChimpRef *left = CHIMP_ARRAY_ITEM(args, 0);
+    ChimpRef *right = CHIMP_ARRAY_ITEM(args, 1);
+
+    r = chimp_object_cmp (left, right);
+
+    if (r == CHIMP_CMP_ERROR) {
+        return NULL;
+    }
+    if (r != CHIMP_CMP_EQ) {
+        fprintf (stderr, "assertion failed: expected %s to be equal to %s\n",
+            CHIMP_STR_DATA(chimp_object_str(left)),
+            CHIMP_STR_DATA(chimp_object_str(right)));
+        exit(1);
+    }
+    else {
+        return chimp_nil;
+    }
+}
+
+static ChimpRef *
+_chimp_test_not_equals (ChimpRef *self, ChimpRef *args)
+{
+    ChimpCmpResult r;
+    ChimpRef *left = CHIMP_ARRAY_ITEM(args, 0);
+    ChimpRef *right = CHIMP_ARRAY_ITEM(args, 1);
+
+    r = chimp_object_cmp (left, right);
+
+    if (r == CHIMP_CMP_ERROR) {
+        return NULL;
+    }
+    if (r == CHIMP_CMP_EQ) {
+        fprintf (stderr, "assertion failed: expected %s to be not equal to %s\n",
+            CHIMP_STR_DATA(chimp_object_str(left)),
+            CHIMP_STR_DATA(chimp_object_str(right)));
+        exit(1);
+    }
+    else {
+        return chimp_nil;
+    }
+}
+
+static ChimpRef *
+_chimp_test_fail (ChimpRef *self, ChimpRef *args)
+{
+    fprintf (stderr, "assertion failed: %s\n",
+        CHIMP_STR_DATA(CHIMP_TEST_NAME(self)));
     exit(1);
     return NULL;
 }
@@ -48,15 +97,18 @@ chimp_test_class_bootstrap (void)
     if (chimp_test_class == NULL) {
         return CHIMP_FALSE;
     }
-    chimp_gc_make_root (NULL, chimp_test_class);
     CHIMP_CLASS(chimp_test_class)->init = _chimp_test_init;
-    chimp_class_add_native_method (chimp_test_class, "fail", chimp_test_fail);
+    chimp_gc_make_root (NULL, chimp_test_class);
+    //chimp_class_add_native_method (chimp_test_class, "eq",     _chimp_test_equals);
+    chimp_class_add_native_method (chimp_test_class, "fail",       _chimp_test_fail);
+    chimp_class_add_native_method (chimp_test_class, "not_equals", _chimp_test_not_equals);
     return CHIMP_TRUE;
 }
 
 ChimpRef *
-chimp_test_new ()
+chimp_test_new (ChimpRef *name)
 {
     ChimpRef* ref = chimp_class_new_instance (chimp_test_class, NULL);
+    CHIMP_TEST(ref)->name = name;
     return ref;
 }
